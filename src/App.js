833ca32955bc4317c6b6473751529d561d758232
Aditya Shukla <adityashukla74@gmail.com>
2023-06-23 15:42:28 +0530

    first commit with i18n

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { useTranslation } from "react-i18next";
+
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  const { t } = useTranslation();
+
+  return <h2>{t('welcome_to_react')}</h2>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './index.css';
 import App from './App';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    fallbackLng: "en",
+    detection: {
+      order: ['cookie', 'htmlTag', 'querystring', 'localStorage', 'sessionStorage', 'path', 'subdomain'],
+      caches: ['cookie']
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: { useSuspense: false }
+  });
 
 ReactDOM.render(
   <React.StrictMode>
